90892dd72b50582f1a2f0beb1534a1bcc0435375
Pedro <nebewu@gmail.com>
2022-06-01 23:35:52 -0300

    Implement user edit and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users, only: %i[new create]
+  resource :users, except: %i[index show destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
