2604018e94aa9e5c03247f4985891945ba72bb00
Pedro <nebewu@gmail.com>
2022-06-02 21:14:57 -0300

    Add user#edit view, a form partial and error messages to the forn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users, except: %i[index show destroy]
+  resources :users, except: %i[index show destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
